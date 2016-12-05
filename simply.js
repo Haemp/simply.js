5e509f93ac2be2453daf2b83dd64b0521b053800
Hampus Ahlgren <wcw106@gmail.com>
2016-12-05 20:48:22 -0300

    added trigger

--- simply.js
+++ simply.js
@@ -1,15 +1,15 @@
-(function (window, define) {
+(function(window, define){
 
-    class Simply {
+    class Simply{
 
-        static get functions() {
+        static get functions(){
             return {
                 '$repeat': Simply.repeat,
                 '$show': Simply.show
             };
         }
 
-        static proxify(obj) {
+        static proxify(obj){
             obj = obj || {};
 
             return new Proxy(Object.assign(obj, {
@@ -21,7 +21,7 @@
                 },
 
                 on(prop, listenerFunction){
-                    if (!this.observers[prop]) {
+                    if(!this.observers[prop]){
                         this.observers[prop] = [];
                     }
 
@@ -29,7 +29,7 @@
                 },
 
                 trigger(prop, value){
-                    if (this.observers[prop]) {
+                    if(this.observers[prop]){
                         this.observers[prop].forEach((listenerFunction) => {
                             console.log('triggering change');
                             listenerFunction(value);
@@ -46,29 +46,29 @@
             })
         }
 
-        static show(el, show) {
-            if (!show) {
+        static show(el, show){
+            if(!show){
                 el.style.display = 'none';
-            } else {
+            }else{
                 el.style.display = 'block';
             }
         }
 
-        static compileRef({node, element, attrName}) {
+        static compileRef({node, element, attrName}){
             const cleanName = Simply.snakeToCamel(attrName.replace(/^#/, ''));
             node[cleanName] = element;
             element.removeAttribute(attrName);
             element.setAttribute('element-id', cleanName);
         }
 
-        static evaluate(expression, context, propertyValue, element) {
+        static evaluate(expression, context, propertyValue, element){
 
             expression = expression.trim();
             var semi = expression.lastIndexOf(';');
-            if (semi === -1) {
+            if(semi === -1){
                 expression = 'return ' + expression;
-            } else {
-                expression = expression.slice(0, semi) + '; return (' + expression.substr(semi + 1) + ')';
+            }else{
+                expression = expression.slice(0, semi) + '; return (' + expression.substr(semi+1) + ')';
             }
 
             let cleanExec = expression.replace(/#/g, 'this.');
@@ -76,11 +76,11 @@
             return f.apply(context, [propertyValue, element]);
         }
 
-        static compileCauseEffect({node, element, attrName, attrValue}) {
+        static compileCauseEffect({node, element, attrName, attrValue}){
             let expression = attrValue;
             const cleanAttr = Simply.snakeToCamel(attrName.replace(/[\(\)]/g, ''));
 
-            if (!expression) {
+            if(!expression){
                 expression = "$el.innerText = $value || ''";
             }
 
@@ -88,26 +88,24 @@
             // the target into account
             if (cleanAttr.includes('.')) {
 
-
-
                 let [target, property] = cleanAttr.split('.');
 
                 // if we're using $self we refer to the container
                 // element root as the base for the property lookup.
                 // this requires the node to be a Model
                 // HTML elements uses events and models uses propety assignments
-                if (node[target] instanceof HTMLElement) {
+                if(node[target] instanceof HTMLElement){
                     node[target].addEventListener(property, (event) => {
                         Simply.evaluate(expression, node, event, element);
                     })
-                } else {
+                }else{
 
-                    if (!node[target]) {
+                    if(!node[target]){
                         console.info('Auto Declaring model for: ', target, ' on ', node);
                         node[target] = Simply.proxify(property === 'length' ? [] : {});
                     }
 
-                    if (!node[target].$isProxy) {
+                    if(!node[target].$isProxy){
                         console.info('Auto Wrapping model for: ', target, ' on ', node);
                         node[target] = Simply.proxify(node[target]);
                     }
@@ -117,16 +115,16 @@
                     });
 
                     // trigger to get the first state of the element
-                    if (node[target][property]) {
+                    if(node[target][property]){
                         Simply.evaluate(expression, node, node[target][property], element);
                     }
                 }
 
-            } else {
+            }else{
 
-                if (cleanAttr === '$compiled') {
+                if(cleanAttr === '$compiled'){
                     Simply.evaluate(expression, node, event, element);
-                } else {
+                }else{
                     element.addEventListener(cleanAttr, (event) => {
                         Simply.evaluate(expression, node, event, element);
                     })
@@ -134,13 +132,13 @@
             }
 
             element.removeAttribute(attrName);
-            element.setAttribute('data-cause-effect', attrName + '="' + expression + '"');
+            element.setAttribute('data-cause-effect', attrName + '="' + expression+ '"');
         }
 
-        static repeat(node, array, dataName) {
+        static repeat(node, array, dataName){
             // node === $el
 
-            if (!node.trigger) {
+            if(!node.trigger){
                 node.trigger = (eventName, data) => {
                     const event = new Event(eventName);
                     event.data = data;
@@ -151,37 +149,37 @@
             // fetch current element
             const elements = Array.prototype.slice.call(node.children).slice(1);
 
-            if (elements.length === array.length) {
+            if(elements.length === array.length){
                 // check if reordered
 
 
-            } else if (elements.length > array.length) {
+            }else if(elements.length > array.length){
                 // remove element mode
 
                 // what do we add?
                 // where do we add it - element order should follow
                 // array order. so array[i] === element[i]
                 let numElements = elements.length;
-                for (let i = 0; i < elements.length; i++) {
+                for(let i = 0; i < elements.length; i++){
 
                     // there is an element registered without
                     // a data item present
-                    if (elements[i][dataName] !== array[i]) {
+                    if(elements[i][dataName] !== array[i]){
                         elements[i].remove();
                         elements.splice(i, 1);
                         i--;
-                    } else {
+                    }else{
                         elements[i].$index = i;
                     }
                 }
-            } else {
+            }else{
                 // add element mode
                 const tmpl = node.querySelector('template');
-                for (let i = 0; i < array.length; i++) {
+                for(let i = 0; i < array.length; i++){
 
                     // there is a data item registered
                     // without an element
-                    if (!elements[i] || array[i] !== elements[i][dataName]) {
+                    if(!elements[i] || array[i] !== elements[i][dataName]){
 
                         // wrapper for to behave as #/this for the
                         // template
@@ -191,7 +189,7 @@
 
                         // we prepopulate the element with the model
                         // value so it is there when we compile
-                        if (!array[i].$isProxy) array[i] = Simply.proxify(array[i]);
+                        if(!array[i].$isProxy) array[i] = Simply.proxify(array[i]);
                         wrapper[dataName] = array[i];
 
                         // give the items access to the parent
@@ -199,9 +197,9 @@
                         wrapper.$repeatEl = node;
                         wrapper.$index = i;
 
-                        if (elements.length === 0) {
+                        if(elements.length === 0){
                             node.appendChild(wrapper);
-                        } else {
+                        }else{
                             node.children[i].insertAdjacentElement('afterend', wrapper);
                         }
 
@@ -211,34 +209,34 @@
             }
         }
 
-        static snakeToCamel(str) {
-            return str.replace(/-([a-z])/g, function (g) {
-                return g[1].toUpperCase();
-            });
+        static snakeToCamel(str){
+            return str.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
         }
 
-        static compileFunction({node, element, attrName}) {
-            element[attrName] = function () {
+        static compileFunction({node, element, attrName}){
+            element[attrName] = function(){
                 Simply.functions[attrName].apply(this, [element, ...arguments]);
             }
         }
 
-        static compile(node) {
+        static compile(node){
 
             const refElements = [];
             const causeEffectElements = [];
             const functionElements = [];
+            const allElements = [];
 
             Array.prototype.slice.apply(node.querySelectorAll('*')).forEach((element) => {
 
                 // check if we have an attribute that is a watch
-                if (element.attributes) {
+                if(element.attributes){
+                    allElements.push(element);
                     Array.prototype.slice.apply(element.attributes).forEach((attr) => {
                         const attrName = attr.name;
                         const attrValue = attr.value;
 
                         // check if identifier
-                        if (attrName.match(/^#/)) {
+                        if(attrName.match(/^#/)){
                             return refElements.push({element, attrName});
                         }
 
@@ -248,7 +246,7 @@
                         }
 
                         // check if function
-                        if (attrName.match(/^\$/)) {
+                        if(attrName.match(/^\$/)){
                             return functionElements.push({element, attrName, attrValue});
                         }
                     });
@@ -267,15 +265,25 @@
                 Simply.compileCauseEffect({node, element, attrName, attrValue});
             });
 
+            allElements.forEach((element) => {
+
+                element.trigger = (eventName, data) => {
+                    const event = new Event(eventName);
+                    event.data = data;
+                    node.dispatchEvent(event);
+                };
+
+                element.trigger('compile');
+            })
         }
     }
 
-    if (!define) {
+    if(!define){
         window.Simply = Simply;
-    } else {
-        define(function () {
+    }else{
+        define(function(){
             return Simply;
         })
     }
 
-})(this, define);
+})(this, this.define);
